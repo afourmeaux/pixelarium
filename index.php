f934c3e2eb4cbd41cac57eba3b0ced8b70a4cf0f
afourmeaux <a.fourmeaux@happy-technologies.com>
2017-01-31 23:23:15 +0100

    starting nav

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
+<?php 
+	$pixelarium = (object) array(
+		'name' => 'Pixelarium',
+		'link' => 'http://' . $_SERVER['HTTP_HOST'] . '/pixelarium',
+	);
+
+	$projects = (object) array(
+		'rollTheDice' => (object) array(
+			'name' => 'Roll the dice',
+			'link' =>  $pixelarium->link . '/projects/rollTheDice/index.php'
+		),
+		'mangaList' => (object) array(
+			'name' => 'Mangas list',
+			'link' =>  $pixelarium->link . '/projects/mangaList/index.php'
+		)
+	);
+ ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Pixelarium</title>
+    <title><?php echo $pixelarium->name ?></title>
     <link rel="stylesheet" href="css/pixelarium.css">
 </head>
 <body>
-    
+    <header>
+    	<a href="<?php echo $pixelarium->link ?>" alt="Aller à l'accueil">
+    		<img src="img/logo" alt="<?php echo $pixelarium->name ?>">
+    	</a>
+    	<nav>
+    		<ul>
+    			<?php foreach ($projects as $project) { ?>
+    				<li>
+    					<a href="<?php echo $project->link ?>">
+    						<?php echo $project->name ?>
+    					</a>
+    				</li>
+    			<?php } ?>
+    		</ul>
+    	</nav>
+    </header>
 </body>
 </html>
