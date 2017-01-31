a5eb656326443ac23f97e6d288db84a6034baf9f
afourmeaux <a.fourmeaux@happy-technologies.com>
2017-01-31 13:31:29 +0100

    init project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Pixelarium</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <h1>Pixelarium</h1>
+</body>
+</html>
